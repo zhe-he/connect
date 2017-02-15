d5e2852f8b1188ac86407281142a7cb27cfc8980
luonan <luonan@ihangmei.com>
2017-02-15 13:14:47 +0800

    event tracking

--- web/js/page/wifi_help.js
+++ web/js/page/wifi_help.js
@@ -1,1 +1,18 @@
 import "../../css/wifi_help.scss";
+
+
+let qrcode = "none"
+let url = window.location.pathname.split("/")
+if (url.length==4 && url[1]=="c") {
+	qrcode = url[2]
+}
+
+
+_paq.push(['trackPageView','home', {dimensionQrCode: qrcode}]);
+
+document.getElementById("download").onclick = ()=>{
+
+	_paq.push([ 'trackEvent', 'Home', 'Click', '', '', {dimensionQrCode: qrcode} ]);
+	document.getElementById("download").style.display="none";
+
+}
